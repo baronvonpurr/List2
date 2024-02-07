ab1bae52af2a51b9cb417ace7f34392d736970ef
baronvonpurr <robinhood.svensson@hotmail.com>
2024-02-07 15:37:33 +0100

    Add project files.

---
+++ List2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace List2
+{
+    class Program
+    {
+        static int Max(List<int> numbers)
+        {
+            numbers.Sort();
+            return numbers[numbers.Count - 1];
+        }
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(142);
+            numbers.Add(34);
+            numbers.Add(376);
+            numbers.Add(245);
+            int big = Max(numbers);
+            Console.WriteLine(big);
+        }
+    }
+}
